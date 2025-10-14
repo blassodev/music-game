6c9a8f11a41e06bacd4c736e69f9901ebd046e15
Blas Santomé Ocampo <bsantome@mercadona.es>
2025-10-14 14:47:30 +0200

    first commit

--- .next/static/chunks/app_admin_decks_page_tsx_c53f1128._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_9568b311._.js"
-],
-    source: "dynamic"
-});
